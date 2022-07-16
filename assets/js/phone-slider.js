0cd7092bef0920223ff7666eaa84650a6a6c6518
oludare89 <azizoyekan@hotmail.com>
2022-07-16 10:56:15 +0000

    Added intervals to slide functions to reduce tapping of arrow keys during gameplay

--- assets/js/phone-slider.js
+++ assets/js/phone-slider.js
@@ -1,18 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     const slider = document.querySelector('.phone-slider')
     let position = 0
+    let isGoingRight = false
+    let isGoingLeft = false
+    speed = 20 // can determine speed of slider
 
     function slideRight() {
-        position += 15
+        isGoingRight = true
+        let rightTimerID = setInterval( function () {
+            position += 15
         slider.style.left = position + 'px'
+        }, speed)
     }
-    slideRight()
 
     function slideLeft() {
-        position -= 15
+        isGoingLeft = true
+        let leftTimerID = setInterval( function () {
+            position -= 15
         slider.style.left = position + 'px'
+        }, speed)
     }
-    slideLeft()
 
     // assign functions to keys
     function control(e) {
