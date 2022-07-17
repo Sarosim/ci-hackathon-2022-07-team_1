d9e78735633fe93b72d9e3e82c40649a2f0525ab
MiaRasmussen05 <rasmussenm376@gmail.com>
2022-07-17 17:17:36 +0000

    Add instructions modal

---
+++ assets/js/instructions-modal.js
@@ -0,0 +1,13 @@
+const instructionsBtn = document.querySelector("#instructions-btn");
+const instructionsModal = document.querySelector(".instructions-modal");
+const closeInstructionsBtn = document.querySelector("#close-instructions");
+
+function openInstructions(){
+    instructionsModal.style.display = "block";
+}
+function closeInstructions(){
+    instructionsModal.style.display = "none";
+}
+instructionsBtn.addEventListener("click", openInstructions);
+closeInstructionsBtn.addEventListener("click", closeInstructions);
+
